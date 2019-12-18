70fd33053a962a3fa8bf92a617570d03ca7c0f94
lyoz <lyoz@users.noreply.github.com>
2019-12-18 19:07:24 +0900

    section 8

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -15,8 +15,22 @@
  */
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async user => {
+  console.log('A new user signed in for the first time.')
+  const fullName = user.displayName || 'Anonymous'
+  await admin.firestore().collection('messages').add({
+    name: 'Firebase Bot',
+    profilePicUrl: '/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!`,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  })
+  console.log('Welcome message written to database.')
+})
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
